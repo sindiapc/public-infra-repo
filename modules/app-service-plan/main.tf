f50baa98ee2bb40a2fce95a2abf01d25b8cb2460
Sindia Choolackal <sindia.choolackal@marlabs.com>
2025-03-28 19:09:34 +0530

    initial

--- modules/app-service-plan/main.tf
+++ modules/app-service-plan/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "zemter" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku_name            =var.sku
-  os_type             = azurerm_service_plan.zemter.os_type  
+  os_type             = "Linux" 
 
   tags = var.tags
 }
