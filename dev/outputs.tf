349f6c1f9e57e2a709131e6506651e1504452b7c
Sindia Choolackal <sindia.choolackal@marlabs.com>
2025-03-28 19:02:13 +0530

    initial

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -3,11 +3,7 @@ output "resource_group_name" {
   description = "The name of the resource group created for the backend."  
 }  
   
-output "app_service_plan_id" {  
-  value       = module.app_service_plan.id  
-  description = "The ID of the App Service Plan created by the module."  
-}  
-  
+
 output "app_service_plan_name" {  
   value       = module.app_service_plan.name  
   description = "The name of the App Service Plan created by the module."  
